c6c9b8ab1393aa6f9de29b1f18648ccc4eb69656
Pavel-Zhukoff <pavel.zhuko.ff@yandex.ru>
2020-11-08 18:22:16 +0300

    fiz static

--- videochat/urls.py
+++ videochat/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from .views import room, room_list
 
@@ -5,3 +6,5 @@ urlpatterns = [
     path('room/', room_list, name='room-list'),
     path('room/<uuid:room_id>', room, name='room'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
