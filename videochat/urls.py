d0bc800675b82f39c8ee97da3daa59f211c0c3ce
Pavel-Zhukoff <pavel.zhuko.ff@yandex.ru>
2020-11-08 19:35:13 +0300

    fix settings

--- videochat/urls.py
+++ videochat/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
+
+from config import settings
 from .views import room, room_list
 
 urlpatterns = [
@@ -7,4 +10,4 @@ urlpatterns = [
     path('room/<uuid:room_id>', room, name='room'),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
