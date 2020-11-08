70104ad101ca19ac70196eab0f78a9d759ad8668
Pavel-Zhukoff <pavel.zhuko.ff@yandex.ru>
2020-11-08 21:48:30 +0300

    somefixes

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-const socket = io('http://frozen-cliffs-23611.herokuapp.com:5000')
+const socket = io('//frozen-cliffs-23611.herokuapp.com:5000')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(null, {
   host: 'peerjs-pavel-zhukoff.herokuapp.com',
